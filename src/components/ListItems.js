82781050c700cdfb661ce7d2ea3f1d43a3e91927
Ricardo Cardoso <ricardo.cardoso@certponto.com.br>
2017-06-21 22:01:01 -0300

    Create component ListItems

---
+++ src/components/ListItems.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export class ListItems extends Component {
+  render() {
+    return (
+      <Text>Teste de importação de componente</Text>
+    );
+  }
+}
